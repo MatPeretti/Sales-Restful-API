4e77119cc370067dc0260b122fc7fc6994d28076
MatPeretti <matheusp2001@gmail.com>
2023-08-14 00:02:29 -0300

    feat: Client controller added

---
+++ src/main/java/com.restfulapi.sales/controller/ClientController.java
@@ -0,0 +1,37 @@
+package com.restfulapi.sales.controller;
+
+import com.restfulapi.sales.domain.entity.Client;
+import com.restfulapi.sales.domain.repository.Clients;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/client")
+public class ClientController {
+
+    public Clients clients;
+
+    public ClientController(Clients clients){
+        this.clients = clients;
+    }
+
+    @GetMapping("{id}")
+    @ResponseBody
+    public ResponseEntity getClientById(@PathVariable Integer id ){
+        Optional<Client> client = clients.findById(id);
+
+        if(client.isPresent()){
+            return ResponseEntity.ok( client.get() );
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+
+}
---
+++ src/main/java/com.restfulapi.sales/domain/entity/Product.java
@@ -0,0 +1,48 @@
+package com.restfulapi.sales.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal price;
+
+    public Product() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
