6a8373320825fb56dbde56f3395af424e0d30ac0
MatPeretti <matheusp2001@gmail.com>
2023-08-15 23:32:26 -0300

    feat: Product Controller added

---
+++ src/main/java/com/restfulapi/sales/controller/ProductController.java
@@ -0,0 +1,67 @@
+package com.restfulapi.sales.controller;
+
+import com.restfulapi.sales.domain.entity.Product;
+import com.restfulapi.sales.domain.repository.Products;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    public Products products;
+
+    public ProductController(Products products) { this.products = products; }
+
+    @GetMapping
+    public List<Product> find(Product filter) {
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filter, matcher);
+        return products.findAll(example);
+    }
+
+    @GetMapping("{id}")
+    public Product getProductById(@PathVariable Integer id) {
+        return products
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product save(@RequestBody Product product) {
+        return products.save(product);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        products.findById(id)
+                .map(product -> {
+                    products.delete(product);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Client not found"));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Integer id, @RequestBody Product product) {
+        products.findById(id)
+                .map(existingProduct -> {
+                    product.setId(existingProduct.getId());
+                    products.save(product);
+                    return existingProduct;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+    }
+
+}
