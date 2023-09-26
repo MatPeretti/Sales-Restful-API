19cd6dc476c64baf9bcc8546201cf19241ea98fc
MatPeretti <matheusp2001@gmail.com>
2023-09-25 23:04:30 -0300

    feat: Added PostgreSQL configuration, User entity, roles enums, UserRepository, and JwtAuthenticationFilter class

---
+++ src/main/java/com/restfulapi/sales/domain/entity/User.java
@@ -0,0 +1,67 @@
+package com.restfulapi.sales.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.restfulapi.sales.domain.enums.UserRole;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_information")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/restfulapi/sales/domain/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.restfulapi.sales.domain.enums;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
